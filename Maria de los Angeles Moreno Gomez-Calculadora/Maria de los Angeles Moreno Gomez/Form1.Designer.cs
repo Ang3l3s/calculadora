5324562e5b36a0d134a21af8fd200c758f3e6af9
Angeles Moreno <freshita_21@hotmail.com>
2017-02-07 23:49:10 -0600

    Add files via upload

---
+++ Maria de los Angeles Moreno Gomez-Calculadora/Maria de los Angeles Moreno Gomez/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Maria_de_los_Angeles_Moreno_Gomez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pantalla = new System.Windows.Forms.TextBox();
+            this.nueve = new System.Windows.Forms.Button();
+            this.ocho = new System.Windows.Forms.Button();
+            this.siete = new System.Windows.Forms.Button();
+            this.seis = new System.Windows.Forms.Button();
+            this.cinco = new System.Windows.Forms.Button();
+            this.cuatro = new System.Windows.Forms.Button();
+            this.tres = new System.Windows.Forms.Button();
+            this.dos = new System.Windows.Forms.Button();
+            this.uno = new System.Windows.Forms.Button();
+            this.suma = new System.Windows.Forms.Button();
+            this.resta = new System.Windows.Forms.Button();
+            this.multiplicacion = new System.Windows.Forms.Button();
+            this.division = new System.Windows.Forms.Button();
+            this.igual = new System.Windows.Forms.Button();
+            this.cero = new System.Windows.Forms.Button();
+            this.punto = new System.Windows.Forms.Button();
+            this.limpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pantalla
+            // 
+            this.pantalla.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pantalla.Location = new System.Drawing.Point(12, 12);
+            this.pantalla.Name = "pantalla";
+            this.pantalla.Size = new System.Drawing.Size(180, 22);
+            this.pantalla.TabIndex = 0;
+            this.pantalla.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.pantalla.TextChanged += new System.EventHandler(this.pantalla_TextChanged);
+            // 
+            // nueve
+            // 
+            this.nueve.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nueve.Location = new System.Drawing.Point(12, 38);
+            this.nueve.Name = "nueve";
+            this.nueve.Size = new System.Drawing.Size(41, 23);
+            this.nueve.TabIndex = 1;
+            this.nueve.Text = "9";
+            this.nueve.UseVisualStyleBackColor = true;
+            this.nueve.Click += new System.EventHandler(this.nueve_Click);
+            // 
+            // ocho
+            // 
+            this.ocho.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ocho.Location = new System.Drawing.Point(59, 38);
+            this.ocho.Name = "ocho";
+            this.ocho.Size = new System.Drawing.Size(41, 23);
+            this.ocho.TabIndex = 2;
+            this.ocho.Text = "8";
+            this.ocho.UseVisualStyleBackColor = true;
+            this.ocho.Click += new System.EventHandler(this.ocho_Click);
+            // 
+            // siete
+            // 
+            this.siete.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siete.Location = new System.Drawing.Point(106, 38);
+            this.siete.Name = "siete";
+            this.siete.Size = new System.Drawing.Size(41, 23);
+            this.siete.TabIndex = 3;
+            this.siete.Text = "7";
+            this.siete.UseVisualStyleBackColor = true;
+            this.siete.Click += new System.EventHandler(this.siete_Click);
+            // 
+            // seis
+            // 
+            this.seis.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seis.Location = new System.Drawing.Point(12, 67);
+            this.seis.Name = "seis";
+            this.seis.Size = new System.Drawing.Size(41, 23);
+            this.seis.TabIndex = 4;
+            this.seis.Text = "6";
+            this.seis.UseVisualStyleBackColor = true;
+            this.seis.Click += new System.EventHandler(this.seis_Click);
+            // 
+            // cinco
+            // 
+            this.cinco.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cinco.Location = new System.Drawing.Point(59, 67);
+            this.cinco.Name = "cinco";
+            this.cinco.Size = new System.Drawing.Size(41, 23);
+            this.cinco.TabIndex = 5;
+            this.cinco.Text = "5";
+            this.cinco.UseVisualStyleBackColor = true;
+            this.cinco.Click += new System.EventHandler(this.cinco_Click);
+            // 
+            // cuatro
+            // 
+            this.cuatro.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuatro.Location = new System.Drawing.Point(106, 67);
+            this.cuatro.Name = "cuatro";
+            this.cuatro.Size = new System.Drawing.Size(41, 23);
+            this.cuatro.TabIndex = 6;
+            this.cuatro.Text = "4";
+            this.cuatro.UseVisualStyleBackColor = true;
+            this.cuatro.Click += new System.EventHandler(this.cuatro_Click);
+            // 
+            // tres
+            // 
+            this.tres.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tres.Location = new System.Drawing.Point(12, 96);
+            this.tres.Name = "tres";
+            this.tres.Size = new System.Drawing.Size(41, 23);
+            this.tres.TabIndex = 7;
+            this.tres.Text = "3";
+            this.tres.UseVisualStyleBackColor = true;
+            this.tres.Click += new System.EventHandler(this.tres_Click);
+            // 
+            // dos
+            // 
+            this.dos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dos.Location = new System.Drawing.Point(59, 96);
+            this.dos.Name = "dos";
+            this.dos.Size = new System.Drawing.Size(41, 23);
+            this.dos.TabIndex = 8;
+            this.dos.Text = "2";
+            this.dos.UseVisualStyleBackColor = true;
+            this.dos.Click += new System.EventHandler(this.dos_Click);
+            // 
+            // uno
+            // 
+            this.uno.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uno.Location = new System.Drawing.Point(106, 96);
+            this.uno.Name = "uno";
+            this.uno.Size = new System.Drawing.Size(41, 23);
+            this.uno.TabIndex = 9;
+            this.uno.Text = "1";
+            this.uno.UseVisualStyleBackColor = true;
+            this.uno.Click += new System.EventHandler(this.uno_Click);
+            // 
+            // suma
+            // 
+            this.suma.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.suma.Location = new System.Drawing.Point(153, 38);
+            this.suma.Name = "suma";
+            this.suma.Size = new System.Drawing.Size(41, 23);
+            this.suma.TabIndex = 10;
+            this.suma.Text = "+";
+            this.suma.UseVisualStyleBackColor = true;
+            this.suma.Click += new System.EventHandler(this.suma_Click);
+            // 
+            // resta
+            // 
+            this.resta.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resta.Location = new System.Drawing.Point(153, 67);
+            this.resta.Name = "resta";
+            this.resta.Size = new System.Drawing.Size(41, 23);
+            this.resta.TabIndex = 11;
+            this.resta.Text = "-";
+            this.resta.UseVisualStyleBackColor = true;
+            this.resta.Click += new System.EventHandler(this.resta_Click);
+            // 
+            // multiplicacion
+            // 
+            this.multiplicacion.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multiplicacion.Location = new System.Drawing.Point(151, 96);
+            this.multiplicacion.Name = "multiplicacion";
+            this.multiplicacion.Size = new System.Drawing.Size(41, 23);
+            this.multiplicacion.TabIndex = 12;
+            this.multiplicacion.Text = "x";
+            this.multiplicacion.UseVisualStyleBackColor = true;
+            this.multiplicacion.Click += new System.EventHandler(this.multiplicacion_Click);
+            // 
+            // division
+            // 
+            this.division.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.division.Location = new System.Drawing.Point(151, 125);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(41, 23);
+            this.division.TabIndex = 13;
+            this.division.Text = "/";
+            this.division.UseVisualStyleBackColor = true;
+            this.division.Click += new System.EventHandler(this.division_Click);
+            // 
+            // igual
+            // 
+            this.igual.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.igual.Location = new System.Drawing.Point(12, 154);
+            this.igual.Name = "igual";
+            this.igual.Size = new System.Drawing.Size(180, 23);
+            this.igual.TabIndex = 14;
+            this.igual.Text = "=";
+            this.igual.UseVisualStyleBackColor = true;
+            this.igual.Click += new System.EventHandler(this.igual_Click);
+            // 
+            // cero
+            // 
+            this.cero.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cero.Location = new System.Drawing.Point(59, 125);
+            this.cero.Name = "cero";
+            this.cero.Size = new System.Drawing.Size(41, 23);
+            this.cero.TabIndex = 15;
+            this.cero.Text = "0";
+            this.cero.UseVisualStyleBackColor = true;
+            this.cero.Click += new System.EventHandler(this.cero_Click);
+            // 
+            // punto
+            // 
+            this.punto.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.punto.Location = new System.Drawing.Point(106, 125);
+            this.punto.Name = "punto";
+            this.punto.Size = new System.Drawing.Size(41, 23);
+            this.punto.TabIndex = 16;
+            this.punto.Text = ".";
+            this.punto.UseVisualStyleBackColor = true;
+            this.punto.Click += new System.EventHandler(this.punto_Click);
+            // 
+            // limpiar
+            // 
+            this.limpiar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.limpiar.Location = new System.Drawing.Point(12, 125);
+            this.limpiar.Name = "limpiar";
+            this.limpiar.Size = new System.Drawing.Size(41, 23);
+            this.limpiar.TabIndex = 17;
+            this.limpiar.Text = "CE";
+            this.limpiar.UseVisualStyleBackColor = true;
+            this.limpiar.Click += new System.EventHandler(this.limpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(207, 191);
+            this.Controls.Add(this.limpiar);
+            this.Controls.Add(this.punto);
+            this.Controls.Add(this.cero);
+            this.Controls.Add(this.igual);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.multiplicacion);
+            this.Controls.Add(this.resta);
+            this.Controls.Add(this.suma);
+            this.Controls.Add(this.uno);
+            this.Controls.Add(this.dos);
+            this.Controls.Add(this.tres);
+            this.Controls.Add(this.cuatro);
+            this.Controls.Add(this.cinco);
+            this.Controls.Add(this.seis);
+            this.Controls.Add(this.siete);
+            this.Controls.Add(this.ocho);
+            this.Controls.Add(this.nueve);
+            this.Controls.Add(this.pantalla);
+            this.Name = "Form1";
+            this.Text = "Calculadora <3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pantalla;
+        private System.Windows.Forms.Button nueve;
+        private System.Windows.Forms.Button ocho;
+        private System.Windows.Forms.Button siete;
+        private System.Windows.Forms.Button seis;
+        private System.Windows.Forms.Button cinco;
+        private System.Windows.Forms.Button cuatro;
+        private System.Windows.Forms.Button tres;
+        private System.Windows.Forms.Button dos;
+        private System.Windows.Forms.Button uno;
+        private System.Windows.Forms.Button suma;
+        private System.Windows.Forms.Button resta;
+        private System.Windows.Forms.Button multiplicacion;
+        private System.Windows.Forms.Button division;
+        private System.Windows.Forms.Button igual;
+        private System.Windows.Forms.Button cero;
+        private System.Windows.Forms.Button punto;
+        private System.Windows.Forms.Button limpiar;
+    }
+}
+
---
+++ Maria de los Angeles Moreno Gomez-Calculadora/Maria de los Angeles Moreno Gomez/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maria_de_los_Angeles_Moreno_Gomez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double valor1, valor2, resultado;
+        string operacion; 
+
+        private void pantalla_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nueve_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "9";
+        }
+
+        private void ocho_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "8";
+        }
+
+        private void siete_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "7";
+        }
+
+        private void seis_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "6";
+        }
+
+        private void cinco_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "5";
+        }
+
+        private void cuatro_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "4";
+        }
+
+        private void tres_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "3";
+        }
+
+        private void dos_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "2";
+        }
+
+        private void uno_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "1";
+        }
+
+        private void suma_Click(object sender, EventArgs e)
+        {
+            valor1 = double.Parse(pantalla.Text);
+            operacion = "+";
+            pantalla.Clear();
+        }
+
+        private void resta_Click(object sender, EventArgs e)
+        {
+            valor1 = double.Parse(pantalla.Text);
+            operacion = "-";
+            pantalla.Clear();
+        }
+
+        private void multiplicacion_Click(object sender, EventArgs e)
+        {
+            valor1 = double.Parse(pantalla.Text);
+            operacion = "x";
+            pantalla.Clear();
+        }
+
+        private void division_Click(object sender, EventArgs e)
+        {
+            valor1 = double.Parse(pantalla.Text);
+            operacion = "/";
+            pantalla.Clear();
+        }
+
+        private void igual_Click(object sender, EventArgs e)
+        {
+            valor2 = double.Parse(pantalla.Text);
+            if (operacion == "+")
+            {
+                resultado = valor1 + valor2;
+            }
+            if (operacion == "-")
+            {
+                resultado = valor1 - valor2;
+            }
+            if (operacion == "/")
+            {
+                resultado = valor1 / valor2;
+            }
+            if (operacion == "x")
+            {
+                resultado = valor1 * valor2;
+            }
+            pantalla.Text = resultado.ToString();
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            pantalla.Clear();
+        }
+
+        private void cero_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + "0";
+        }
+
+        private void punto_Click(object sender, EventArgs e)
+        {
+            pantalla.Text = pantalla.Text + ".";
+        }
+    }
+}
